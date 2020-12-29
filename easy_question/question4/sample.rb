a0f7032b7250aa6ba8bab6a201f78cbb28a2e38a
shodayuki <yshoda1227@gmail.com>
2020-12-29 15:39:42 +0900

    初級問題4実装

---
+++ easy_question/question4/sample.rb
@@ -0,0 +1,11 @@
+## 問題1
+## 文字列"Hello" のクラスを class メソッドを使って出力してみましょう。
+"Hello".class
+
+## 問題2
+## Integer クラスのメソッドを調べる方法をターミナルで実行してみましょう。
+ls Integer
+
+## 問題3
+## "HELLO WORLD" を String クラスのメソッドを使って全て小文字に変換してみましょう。
+"HELLO WORLD".downcase
