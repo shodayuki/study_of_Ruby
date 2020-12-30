bbb6ced8d4026b836060413c69b1ed3221357977
shodayuki <yshoda1227@gmail.com>
2020-12-30 22:43:00 +0900

    ハッシュ初級問題5実装

---
+++ hash_question/question5/sample.rb
@@ -0,0 +1,9 @@
+## 問題5
+## 次の変数 hash のキーをアルファベット順の昇順で、ハッシュのまま並び替えてください。
+## 並び替えた結果を、変数 result に代入してください。
+
+hash = {jun: 202, tomo: 170, ryo: 183, akira: 190, hiro: 174}
+
+result = hash.sort.to_h
+
+p result
